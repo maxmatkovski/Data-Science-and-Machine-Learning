f225840bde95b0a4b15f112541683f4feef0d31a
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-04 02:34:31 -0800

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -14,3 +14,11 @@ np.array(my_mat)
 array([[1, 2, 3],
        [4, 5, 6],
        [7, 8, 9]])
+
+// More array Practice
+
+np.arange(0,11)
+array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+
+np.arange(0,11,2)
+array([ 0,  2,  4,  6,  8, 10])
