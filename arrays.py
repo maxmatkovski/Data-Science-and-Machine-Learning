eeb4e7268113fd1a2444391df33d8884942f7b45
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-04 02:32:01 -0800

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,16 @@
+my_list = [1,2,3]
+
+import numpy as np
+
+arr = np.array(my_list)
+arr
+
+array([1, 2, 3])
+my_mat = [[1,2,3],[4,5,6],[7,8,9]]
+my_mat
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+my_mat
+np.array(my_mat)
+array([[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]])
