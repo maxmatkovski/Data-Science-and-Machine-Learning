4c1fe400142f876f2c93d7119de1268347be7253
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-04 16:13:58 -0800

    Update random_practice.py
    array stuff

--- random_practice.py
+++ random_practice.py
@@ -1,8 +1,36 @@
-// Boolean Arrays
+import numpy as np
+
+arr_2d = np.array([[5,10,15],[20,25,30],[35,40,45]])
+
+arr_2d
+
+arr_2d[0][0]
+
+arr_2d[0]
+
+arr_2d[1][1]
+
+arr_2d[1,1]
+
+arr_2d[2,1]
+
+arr_2d[:2,1:]
+
+arr_2d[1:,1:]
+
+arr = np.arange(1,11)
+
+arr
+
+arr > 5
 
 bool_arr = arr > 5
 
 bool_arr
 
 arr[bool_arr]
+
+arr[arr>5]
+
+arr[arr < 3]
 
